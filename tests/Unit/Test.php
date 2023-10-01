7a666100ac2c0d90d9b97b4ac0d69c89ac4f1981
sergiu <sergiugrigoreturcanu@gmail.com>
2023-10-01 17:04:12 -0400

    DIMP : Set up the unit tests and php-cs-fixer.

---
+++ tests/Unit/Test.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class Test extends TestCase
+{
+    /**
+     * @test
+     * @return void
+     */
+    public function it_should_work_fine(): void
+    {
+        $this->assertTrue(true);
+    }
+}
